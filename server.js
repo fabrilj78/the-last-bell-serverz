02644b4a2a6230253e2320b48edd9214d077da17
fabrilj78 <fabrilj78@gmail.com>
2026-08-19 16:22:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    online: true,
+    game: "The Last Bell",
+    message: "Servidor funcionando"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
+});
